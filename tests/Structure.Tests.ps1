f14aada0c9c7cc21e09cfa7f96949ed98f1359df
lordneeko <lordneeko@users.noreply.github.com>
2021-10-15 15:16:08 -0400

    Update Structure.Tests.ps1

--- tests/Structure.Tests.ps1
+++ tests/Structure.Tests.ps1
@@ -1,11 +1,12 @@
 Describe 'SuperCAT Structure' {
 	Context 'File Structure' {
 		It 'setup_powershell.ps1 exists' {
-			"/setup_powershell.ps1" | Should -Exist
+			$currentDir = [string](Get-Location)
+			$currentDir & "/setup_powershell.ps1" | Should -Exist
 		}
 		It 'current directory' {
 			$currentDir = Get-Location
-			$currentDir | Should -Be "D:\a\SuperCAT\SuperCAT"
+			$currentDir | Should -Be "\SuperCAT\SuperCAT"
 		}
 	}
 }
