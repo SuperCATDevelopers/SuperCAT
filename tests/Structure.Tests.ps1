1641f53e747284c5f2d65ad1f05258963d9768a4
lordneeko <lordneeko@users.noreply.github.com>
2021-10-15 15:01:07 -0400

    Update Structure.Tests.ps1

--- tests/Structure.Tests.ps1
+++ tests/Structure.Tests.ps1
@@ -1,7 +1,7 @@
 Describe 'SuperCAT Structure' {
 	Context 'File Structure' {
 		It 'setup_powershell.ps1 exists' {
-			"setup_powershell.ps1" | Should -Exist
+			"/setup_powershell.ps1" | Should -Exist
 		}
 	}
 }
