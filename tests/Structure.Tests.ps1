d3cbfcf194f1502cf96854dd7e4eb281fef48a29
lordneeko <lordneeko@users.noreply.github.com>
2021-10-15 15:40:08 -0400

    Update Structure.Tests.ps1

--- tests/Structure.Tests.ps1
+++ tests/Structure.Tests.ps1
@@ -1,12 +1,27 @@
 Describe 'SuperCAT Structure' {
 	Context 'File Structure' {
-		It 'SuperCAT files are in the correct location' {
+		It 'Setup file check' {
 			$currentDir = [string](Get-Location)
 			$currentDir + "\setup-powershell.ps1" | Should -Exist
+		}
+		It 'DISA Directory Check' {
+			$currentDir = [string](Get-Location)
 			$currentDir + "\DISA" | Should -Exist
+		}
+		It 'AV Directory Check' {
+			$currentDir = [string](Get-Location)
 			$currentDir + "\AV" | Should -Exist
+		}
+		It 'Export EventLogs check' {
+			$currentDir = [string](Get-Location)
 			$currentDir + "\Export\export-offline-eventlogs.ps1" | Should -Exist
+		}
+		It 'Export Registry Check' {
+			$currentDir = [string](Get-Location)
 			$currentDir + "\Export\export-offline-registry.ps1" | Should -Exist
+		}
+		It 'SuperCAT main file check' {
+			$currentDir = [string](Get-Location)
 			$currentDir + "\Tool\SuperCAT.ps1" | Should -Exist
 		}
 	}
