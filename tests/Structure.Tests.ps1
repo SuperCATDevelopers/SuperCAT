b86ccc5cf4bc375eef3c0dfb959a00895ccfbd99
lordneeko <lordneeko@users.noreply.github.com>
2021-10-19 11:40:09 -0400

    Update Structure.Tests.ps1

--- tests/Structure.Tests.ps1
+++ tests/Structure.Tests.ps1
@@ -6,14 +6,14 @@ Describe 'SuperCAT Structure' {
 		}
 		It 'DISA Directory Check' {
 			$currentDir = [string](Get-Location)
-			$currentDir + "\DISA" | Should -Exist
+			$currentDir + "\Tool\DISA" | Should -Exist
 		}
 		It 'AV Directory Check' {
 			$currentDir = [string](Get-Location)
-			$currentDir + "\AV" | Should -Exist
-			$currentDir + "\AV\w32" | Should -Exist
-			$currentDir + "\AV\w64" | Should -Exist
-			$currentDir + "\AV\DAT" | Should -Exist
+			$currentDir + "\Tool\AV" | Should -Exist
+			$currentDir + "\Tool\AV\w32" | Should -Exist
+			$currentDir + "\Tool\AV\w64" | Should -Exist
+			$currentDir + "\Tool\AV\DAT" | Should -Exist
 		}
 		It 'QuickLookTool checks' {
 			$currentDir = [string](Get-Location)
